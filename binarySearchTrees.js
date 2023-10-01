14fd9b89ed04806dcc66c7ad32615b8c20c47e78
Jake Abed <jakeabed@gmail.com>
2023-10-01 00:26:19 -0700

    Initialize project and start setting up structure

---
+++ binarySearchTrees.js
@@ -0,0 +1,15 @@
+'use strict';
+
+//Node class with value, left, and right.
+
+class Node {
+    constructor(data, left = null, right = null) {
+        this.data = data;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+//Tree class that creates binary search trees.
+
+//
